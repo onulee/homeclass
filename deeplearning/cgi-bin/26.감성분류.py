db736ec900681297f61c12e1382af33c8b157ed5
onulee <onulee@naver.com>
2022-07-15 23:22:08 +0900

    commit

---
+++ deeplearning/cgi-bin/26.감성분류.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import warnings
+warnings.filterwarnings("ignore")
+
+df = pd.read_csv("deeplearning/review_data.csv")
+
+print(df)
+
+import re
+
+# 텍스트 정제 함수 : 한글 이외의 문자는 전부 제거
+def text_cleaning(text):
+    # 한글의 정규표현식으로 한글만 추출합니다.
+    hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')
+    result = hangul.sub('', text)
+    return result
+
+df['ko_text'] = df['review'].apply(lambda x: text_cleaning(x))
+del df['review']
+
+# 한 글자 이상의 텍스트를 가지고 있는 데이터만 추출합니다
+df = df[df['ko_text'].str.len() > 0]
+df.head()
+
+
+from konlpy.tag import Okt
+
+# konlpy라이브러리로 텍스트 데이터에서 형태소를 추출합니다.
+def get_pos(x):
+    tagger = Okt()
+    pos = tagger.pos(x)
+    pos = ['{}/{}'.format(word,tag) for word, tag in pos]
+    return pos
+
+# 형태소 추출 동작을 테스트합니다.
+result = get_pos(df['ko_text'].values[0])
+print(result)
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+# 형태소를 벡터 형태의 학습 데이터셋(X 데이터)으로 변환합니다.
+index_vectorizer = CountVectorizer(tokenizer = lambda x: get_pos(x))
+X = index_vectorizer.fit_transform(df['ko_text'].tolist())
+
+from sklearn.feature_extraction.text import TfidfTransformer
+
+# TF-IDF 방법으로, 형태소를 벡터 형태의 학습 데이터셋(X 데이터)으로 변환합니다.
+tfidf_vectorizer = TfidfTransformer()
+X = tfidf_vectorizer.fit_transform(X)
+
+print(X.shape)
+print(X[0])
+
+from sklearn.model_selection import train_test_split
+
+y = df['y']
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+print(x_train.shape)
+print(x_test.shape)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# 로지스틱 회귀모델을 학습합니다.
+lr = LogisticRegression(random_state=0)
+lr.fit(x_train, y_train)
+y_pred = lr.predict(x_test)
+y_pred_probability = lr.predict_proba(x_test)[:,1]
+
+# 로지스틱 회귀모델의 성능을 평가합니다.
+print("accuracy: %.2f" % accuracy_score(y_test, y_pred))
+print("Precision : %.3f" % precision_score(y_test, y_pred))
+print("Recall : %.3f" % recall_score(y_test, y_pred))
+print("F1 : %.3f" % f1_score(y_test, y_pred))
+
+from sklearn.metrics import confusion_matrix
+
+# Confusion Matrix를 출력합니다.
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+
+from sklearn.metrics import roc_curve, roc_auc_score
+
+# AUC를 계산합니다.
+false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred_probability)
+roc_auc = roc_auc_score(y_test, y_pred_probability)
+print("AUC : %.3f" % roc_auc)
+
+# ROC curve 그래프를 출력합니다.
+plt.rcParams['figure.figsize'] = [5, 4]
+plt.plot(false_positive_rate, true_positive_rate, label='ROC curve (area = %0.3f)' % roc_auc, 
+         color='red', linewidth=4.0)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC curve of Logistic regression')
+plt.legend(loc="lower right")
+
+# 학습한 회귀 모델의 계수를 출력합니다.
+plt.rcParams['figure.figsize'] = [10, 8]
+plt.bar(range(len(lr.coef_[0])), lr.coef_[0])
+
+print(sorted(((value, index) for index, value in enumerate(lr.coef_[0])), reverse=True)[:5])
+print(sorted(((value, index) for index, value in enumerate(lr.coef_[0])), reverse=True)[-5:])
+
+# 회귀 모델의 계수를 높은 순으로 정렬합니다. 
+coef_pos_index = sorted(((value, index) for index, value in enumerate(lr.coef_[0])), reverse=True)
+
+# 회귀 모델의 계수를 index_vectorizer에 맵핑하여, 어떤 형태소인지 출력할 수 있게 합니다.
+invert_index_vectorizer = {v: k for k, v in index_vectorizer.vocabulary_.items()}
+
+# 계수가 높은 순으로, 피처에 형태소를 맵핑한 결과를 출력합니다. 계수가 높은 피처는 리뷰에 긍정적인 영향을 주는 형태소라고 할 수 있습니다.
+print(str(invert_index_vectorizer)[:100]+'..')
+
+# 상위 20개 긍정 형태소를 출력합니다.
+for coef in coef_pos_index[:20]:
+    print(invert_index_vectorizer[coef[1]], coef[0])
+    
+    # 상위 20개 부정 형태소를 출력합니다.
+for coef in coef_pos_index[-20:]:
+    print(invert_index_vectorizer[coef[1]], coef[0])
+
+
+
